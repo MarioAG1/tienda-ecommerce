48ace36f3fcfc001e1806921ce26b1b1bb1dfb0f
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2024-01-23 11:13:35 +0100

    Creacion de los servicios

---
+++ src/main/java/com/ecommerce/backend/application/UserService.java
@@ -0,0 +1,22 @@
+package com.ecommerce.backend.application;
+
+import com.ecommerce.backend.domain.model.User;
+import com.ecommerce.backend.domain.port.IUserRepository;
+
+public class UserService {
+    private final IUserRepository iUserRepository;
+
+    public UserService(IUserRepository iUserRepository) {
+        this.iUserRepository = iUserRepository;
+    }
+
+    public User save(User user) {
+        return this.iUserRepository.save(user);
+
+    }
+
+    public User findById(Integer id) {
+        return this.iUserRepository.findById(id);
+    }
+}
+
