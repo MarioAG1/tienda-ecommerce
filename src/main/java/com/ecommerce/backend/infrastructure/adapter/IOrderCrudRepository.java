c05a22edc67d8d8b56647802a866960cf30aad63
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2024-03-25 19:51:34 +0100

    Arreglo en los campos relacionados con la Objeto, falla un campo

--- src/main/java/com/ecommerce/backend/infrastructure/adapter/IOrderCrudRepository.java
+++ src/main/java/com/ecommerce/backend/infrastructure/adapter/IOrderCrudRepository.java
@@ -3,7 +3,6 @@ package com.ecommerce.backend.infrastructure.adapter;
 import com.ecommerce.backend.domain.model.OrderState;
 import com.ecommerce.backend.infrastructure.entity.OrderEntity;
 import com.ecommerce.backend.infrastructure.entity.UserEntity;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +11,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface IOrderCrudRepository extends CrudRepository<OrderEntity, Integer> {
     @Transactional
     @Modifying
-    @Query("UPDATE OrderEntity o SET o.state = :state WHERE  o.id = :id")
+    @Query("UPDATE OrderEntity o SET o.orderState = :state WHERE  o.id = :id")
     void updateStateById(Integer id, OrderState state);
 
     Iterable<OrderEntity> findByUserEntity(UserEntity userEntity);
