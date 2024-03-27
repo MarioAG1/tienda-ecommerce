b3a4db22c3a9c808ba502bea79aeb7962e2e8120
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2024-03-27 18:20:54 +0100

    Cambios en el controlador para la comunicacion con el front

--- src/main/java/com/ecommerce/backend/infrastructure/rest/ProductController.java
+++ src/main/java/com/ecommerce/backend/infrastructure/rest/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/admin/products")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
     private final ProductService productService;
 
